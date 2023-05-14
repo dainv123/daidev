9cd38d6f9d9559498f7a36572f5a8db0226075fb
dainv123 <testofdai@gmail.com>
2023-05-14 11:04:12 +0700

    add table mongod

--- src/server/helper/default-state.js
+++ src/server/helper/default-state.js
@@ -22,7 +22,9 @@ export const DefaultState = {
         avatar: "",
         name: "",
         address: "",
-        greeting: ""
+        greeting: "",
+        email: "",
+        phone: "",
     }],
     setting: [{
         user: "U1",
@@ -96,59 +98,14 @@ export const DefaultState = {
         point: 0,
     }],
     workSkills: [{
-        id: "U1",
+        user: "U1",
         title: "",
         percent: 0
     }],
-    // groups:[{
-    //     name:"To Do",
-    //     id:"G1",
-    //     owner:"U1"
-    // },{
-    //     name:"Doing",
-    //     id:"G2",
-    //     owner:"U1"
-    // },{
-    //     name:"Done",
-    //     id:"G3",
-    //     owner:"U1"
-    // }
-    // ],
-    // tasks:[{
-    //     name:"Refactor tests",
-    //     id:"T1",
-    //     group:"G1",
-    //     owner:"U1",
-    //     isComplete:false,
-    // },{
-    //     name:"Meet with CTO",
-    //     id:"T2",
-    //     group:"G1",
-    //     owner:"U1",
-    //     isComplete:true,
-    // },{
-    //     name:"Compile ES6",
-    //     id:"T3",
-    //     group:"G2",
-    //     owner:"U2",
-    //     isComplete:false,
-    // },{
-    //     name:"Update component snapshots",
-    //     id:"T4",
-    //     group:"G2",
-    //     owner:"U1",
-    //     isComplete:true,
-    // },{
-    //     name:"Production optimizations",
-    //     id:"T5",
-    //     group:"G3",
-    //     owner:"U1",
-    //     isComplete:false,
-    // }],
-    // comments:[{
-    //     owner:"U1",
-    //     id:"C1",
-    //     task:"T1",
-    //     content:"Great work!"
-    // }]
+    socials: [{
+        user: "U1",
+        title: "",
+        icon: "",
+        link: ""
+    }]
 };
